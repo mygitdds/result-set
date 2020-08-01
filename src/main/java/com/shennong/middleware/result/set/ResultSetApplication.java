82fd2b28f8ceadcd3a0c69627f8782b164a3c12e
dengdongsheng1 <997470320@qq.com>
2020-08-01 14:47:54 +0800

    first commit

---
+++ src/main/java/com/shennong/middleware/result/set/ResultSetApplication.java
@@ -0,0 +1,13 @@
+package com.shennong.middleware.result.set;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResultSetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResultSetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shennong/middleware/result/set/ResultSetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shennong.middleware.result.set;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResultSetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
